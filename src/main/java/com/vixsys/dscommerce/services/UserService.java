054d47d4299ba009570fa6bc2ab026fd75b1e5a0
Herhos <herhos@outlook.com>
2024-07-09 09:16:15 -0300

    Obtendo usuário logado

--- src/main/java/com/vixsys/dscommerce/services/UserService.java
+++ src/main/java/com/vixsys/dscommerce/services/UserService.java
@@ -1,12 +1,15 @@
 package com.vixsys.dscommerce.services;
 
+import com.vixsys.dscommerce.dtos.UserDto;
 import com.vixsys.dscommerce.entities.User;
 import com.vixsys.dscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService implements UserDetailsService
@@ -23,5 +26,25 @@ public class UserService implements UserDetailsService
          throw new UsernameNotFoundException("Email not found");
       }
       return user;
+   }
+
+   protected User authenticated()
+   {
+      try
+      {
+         String username = SecurityContextHolder.getContext().getAuthentication().getName();
+         return repository.findByEmail(username);
+      }
+      catch (Exception e)
+      {
+         throw new UsernameNotFoundException("Invalid user");
+      }
+   }
+
+   @Transactional(readOnly = true)
+   public UserDto getMe()
+   {
+      User entity = authenticated();
+      return new UserDto(entity);
    }
 }
